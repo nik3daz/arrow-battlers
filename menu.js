c87dfc0007376787b882f3ff235a72fc46897436
Rhys Davis <rdav870@cse.unsw.EDU.AU>
2012-09-27 17:29:48 +1000

    Framework

---
+++ menu.js
@@ -0,0 +1,13 @@
+function initMenu() {
+    var r = new Kinetic.Rect({
+        width: stage.getWidth(),
+        height: stage.getHeight(),
+        fill: "white",
+    });
+    menuLayer.add(r);
+}
+
+function showMenu() {
+}
+
+
