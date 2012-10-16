b4088bbad8fff602c3b826eef74c24816242b513
Rhys Davis <rhysfdavis@gmail.com>
2012-10-17 00:59:13 +1100

    derp

--- game.js
+++ game.js
@@ -76,17 +76,17 @@ function Player(id, dir, udlre) {
 
         // claim all the free skins for this character
         this.unlockedSkins[characterId] = [];
-        for (var j; j < ClassList.characters[characterId]; j++) {
+        for (var j = 0; j < ClassList.characters[i].skins.length; j++) {
             // check if the skin is free
             this.unlockedSkins[characterId][j] = [];
-            if (ClassList.characters[characterId].skinsCost[characterId] == 0) {
+            if (ClassList.characters[this.unlockedChars[i]].skinsCost[j] == 0) {
                 this.unlockedSkins[characterId][j].push(0);
                 this.unlockedSkins[characterId][j].push(1);
                 this.unlockedSkins[characterId][j].push(2);
             }
         }
 
-        scb();
+            scb();
         } else {
             fcb();
         }
